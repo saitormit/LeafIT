ee88ce7d589fa29848f004c59fd6f8adefa42193
saitormit <mitsuaki.saito04@gmail.com>
2022-11-05 18:37:14 -0400

    pots-config get method now returns static moisture parameters

--- plantech_server.py
+++ plantech_server.py
@@ -15,11 +15,24 @@ def hello():
 
 @app.route("/pots-config", methods=["GET","POST","DELETE"])
 def configure_pots():
+    #Number of pots:
+    potsNum = 2
     #Database Parameters
-    collection = db.plantechDB["UserPlants"]
+    userCollection = db.plantechDB["UserPlants"]
 
     if request.method == "GET":
-        return jsonify({"Message": "GET request received"})
+        moistConfig = {}
+        infoCollection = db.plantechDB["PlantsInfo"]
+        for id in range(1, potsNum+1):
+            if userCollection.count_documents({"_id": id}):
+                plant = userCollection.find_one({"_id": id}).get("Plant")
+                moisture = infoCollection.find_one({"Plant": plant}).get("Moisture")
+                moistConfig["pot"+str(id)] = moisture
+            else:
+                moistConfig["pot"+str(id)] = None
+
+        return jsonify(moistConfig)
+        #return jsonify({"Message": "GET request received"})
     
     elif request.method == "POST":
         #Input received from Android
@@ -29,8 +42,8 @@ def configure_pots():
         plant = userInput["Plant"]
         stage = userInput["Stage"]
 
-        if collection.count_documents({"_id": pot}) == 0:
-            collection.insert_one({"_id": pot, "Plant": plant, "Stage": stage})
+        if userCollection.count_documents({"_id": pot}) == 0:
+            userCollection.insert_one({"_id": pot, "Plant": plant, "Stage": stage})
             return jsonify({"Message": plant + " added at pot " + str(pot) + " succesfully"})
         else:
             return jsonify({"Message": "Pot " + str(pot) + " is already being used"})
@@ -39,15 +52,15 @@ def configure_pots():
         #Input from the user with the ID to be deleted
         empty_pot = int(request.args.get("empty"))
 
-        if collection.count_documents({"_id": empty_pot}) > 0:
-            collection.delete_one({"_id": empty_pot})
+        if userCollection.count_documents({"_id": empty_pot}) > 0:
+            userCollection.delete_one({"_id": empty_pot})
             return jsonify({"Message": "Pot " + str(empty_pot) + " is now empty"})
         else:
             return jsonify({"Message": "Pot " + str(empty_pot) + " is already empty"})
 
 @app.route("/moisture-data", methods=["GET","POST"])
 def manage_moisture():
-    collection = db.plantechDB["MoistureReadings"]
+    userCollection = db.plantechDB["MoistureReadings"]
     return jsonify({"Message": "Moisture data"})
             
 if __name__ == "__main__":
