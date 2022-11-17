a53b1dafe05d6e1b0859e3fbbca9bace53368fa3
saitormit <mitsuaki.saito04@gmail.com>
2022-11-17 01:32:34 -0500

    Method to plot graphs created

--- app/src/main/java/com/example/leafit/MoistureActivity.java
+++ app/src/main/java/com/example/leafit/MoistureActivity.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -16,21 +15,26 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.jjoe64.graphview.GraphView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MoistureActivity extends AppCompatActivity {
 
     private Button btnWater;
     private CheckBox checkboxOne, checkboxTwo;
+    private GraphView plotOne, plotTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moisture);
-        initViews();
         String serverURL = getIntent().getStringExtra("serverURL");
+        initViews();
+        plotGraphs(serverURL, plotOne, plotTwo);
 
         btnWater.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,9 +71,43 @@ public class MoistureActivity extends AppCompatActivity {
         });
     }
 
+    private void plotGraphs(String serverURL, GraphView plotOne, GraphView plotTwo) {
+        String api_URL = serverURL + "/moisture-data";
+        RequestQueue queue = Volley.newRequestQueue(MoistureActivity.this);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, api_URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //Toast.makeText(MoistureActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                //TODO: Loop and Get from response "time" and "moisture" array for each pot.
+                for(int i = 1; i < response.length()+1; i++) {
+                    Object timestampList = new ArrayList<>();
+                    Object moistureList = new ArrayList<>();
+                    try {
+                        timestampList = response.getJSONObject("pot"+i).get("time");
+                        moistureList = response.getJSONObject("pot"+i).get("moisture");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    //Toast.makeText(MoistureActivity.this, timestampList.toString(), Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MoistureActivity.this, "GET request failed", Toast.LENGTH_SHORT).show();
+                Log.e("Error", "GET failed");
+            }
+        });
+        queue.add(request);
+    }
+
     private void initViews() {
         btnWater = findViewById(R.id.waterBtn);
         checkboxOne = findViewById(R.id.checkboxOne);
         checkboxTwo = findViewById(R.id.checkboxTwo);
+        plotOne = findViewById(R.id.plotOne);
+        plotTwo = findViewById(R.id.plotTwo);
     }
 }
